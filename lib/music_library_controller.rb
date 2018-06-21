648205a886fad4f0d16e8507e2dbb7988977ff1a
Christia T <christiatidwell@bellsouth.net>
2018-06-21 01:29:06 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -84,6 +84,7 @@ def play_song
 			song = nil
 			if song_input == 1..Song.all.length
         song = Song.all.sort_by{|s| s.name}.each.with_index(song_input to_i-1) do |song, index|
+					if index == i
 		      puts "#{index}. #{song.artist} - #{song} - #{song.genre}"
 				end
 					puts "Playing #{song} by #{song.artist}"
