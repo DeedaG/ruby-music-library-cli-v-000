e622a1e81c6c32739f1c13205ed117dab0c66e92
Christia T <christiatidwell@bellsouth.net>
2018-06-21 01:22:11 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -83,7 +83,7 @@ def play_song
 			song_input = gets.strip
 			song = nil
 			if song_input == 1..Song.all.length
-				song = Song.all.sort_by{|s| s.name}.each.with_index(song_input to_i) do |song, i|
+				song = Song.all.sort_by{|s| s.name}.each.with_index(song_input to_i-1) do |song, i|
 				if index == "i"
 					puts "Playing #{song} by #{song.artist}"
 				end
