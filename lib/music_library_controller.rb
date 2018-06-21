86befb55aef74b006e56cd3cb6c7c3f675f00e7a
Christia T <christiatidwell@bellsouth.net>
2018-06-21 01:27:26 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -83,12 +83,10 @@ def play_song
 			song_input = gets.strip
 			song = nil
 			if song_input == 1..Song.all.length
-        song = list_songs.uniq
+        song = list_songs
 					puts "Playing #{song} by #{song.artist}"
 
 				end
 
 			end
 		end
-
-	
